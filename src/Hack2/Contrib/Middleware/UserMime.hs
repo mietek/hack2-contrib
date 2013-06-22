5d0fbcd9e66206c9eb78aa07ba5b9b926f7d70e9
Jinjing Wang <nfjinjing@gmail.com>
2013-06-22 12:46:46 +0800

    use latest air

--- src/Hack2/Contrib/Middleware/UserMime.hs
+++ src/Hack2/Contrib/Middleware/UserMime.hs
@@ -14,7 +14,7 @@ import Data.ByteString.Char8 (ByteString)
 user_mime :: [(ByteString, ByteString)] -> Middleware
 user_mime h app env = do
   r <- app env
-  case h.only_fst.find mime >>= flip lookup h of
+  case h.map fst.find mime >>= flip lookup h of
     Nothing -> return r
     Just v -> return - r.set_content_type v
   where mime x = env.path_info.B.unpack.ends_with ('.' : x.B.unpack)
--- src/Hack2/Contrib/Test.hs
+++ src/Hack2/Contrib/Test.hs
@@ -1,4 +1,4 @@
-module Hack2.Contrib.Test where
+module Main where
 
 import  Hack2.Contrib.Constants
 import  Hack2.Contrib.Middleware.BounceFavicon
@@ -11,15 +11,11 @@ import  Hack2.Contrib.Middleware.File
 import  Hack2.Contrib.Middleware.Head
 import  Hack2.Contrib.Middleware.Inspect
 import  Hack2.Contrib.Middleware.NotFound
-import  Hack2.Contrib.Middleware.RegexpRouter
--- import  Hack2.Contrib.Middleware.ShowExceptions
 import  Hack2.Contrib.Middleware.SimpleAccessLogger
--- import  Hack2.Contrib.Middleware.Hub
 import  Hack2.Contrib.Middleware.Static
 import  Hack2.Contrib.Middleware.URLMap
 import  Hack2.Contrib.Middleware.IOConfig
 import  Hack2.Contrib.Middleware.UserMime
--- import  Hack2.Contrib.Middleware.UTF8Body
 import  Hack2.Contrib.Middleware.XForwardedForToRemoteHost
 
 import  Hack2.Contrib.Middleware.Cascade
@@ -27,3 +23,8 @@ import  Hack2.Contrib.Mime
 import  Hack2.Contrib.Request
 import  Hack2.Contrib.Response
 import  Hack2.Contrib.Utils
+
+import System.Nemesis.Titan
+
+main :: IO ()
+main = halt
